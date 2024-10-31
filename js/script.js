99e2bc459731940f85da5a1a0fd75d544b6889e1
kmacedo28 <kaiky.macedo@aluno.senai.br>
2024-10-31 11:04:31 -0300

    aula-31/10/2024

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 const form = document.getElementById('form');
+const name_input = document.getElementById('name-input');
 const email_input = document.getElementById('email-input');
 const password_input = document.getElementById('password-input');
+const repeat_password_input = document.getElementById('repeat-password-input');
 const error_message = document.getElementById('error-message');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let errors = [];
 
-    errors = validateLoginForm(
+    const fileName = e.target.baseURI.split("/").pop();
+
+    if (fileName === "register.html") {
+      errors = validateRegisterForm(
+          name_input.value,
+          email_input.value,
+          password_input.value,
+          repeat_password_input.value
+      ); 
+    } else {
+      errors = validateLoginForm(
         email_input.value, 
         password_input.value
     );
+    }
 
     // Verificar Login
     if (errors.length === 0) {
@@ -48,8 +61,10 @@ function validateLoginForm(email, password) {
 }
 
 const allInputs = [
+    name_input,
     email_input,
     password_input,
+    repeat_password_input
   ].filter((input) => input != null);
   
   allInputs.forEach((input) => {
@@ -83,4 +98,41 @@ function verifyLogin(email, password) {
       console.error("Erro ao buscar usuários:", request.statusText);
       return false;
   }
+}
+
+function validateRegisterForm(name, email, password, repeatPassword) {
+  let errors = [];
+
+  if (name === "" || name == null) {
+    errors.push("Nome é obrigatório");
+    name_input.parentElement.classList.add("incorrect");
+  }
+
+  if (email === "" || email == null) {
+    errors.push("Email é obrigatório");
+    email_input.parentElement.classList.add("incorrect");
+  }
+
+  if (password === "" || password == null) {
+    errors.push("Senha é obrigatória");
+    password_input.parentElement.classList.add("incorrect");
+  } else {
+    if (password.length < 8) {
+      errors.push("Senha deve ter no mínimo 8 caracteres");
+      password_input.parentElement.classList.add("incorrect");
+    }
+  }
+
+  if (repeatPassword === "" || repeatPassword == null) {
+    errors.push("Repita a senha");
+    repeat_password_input.parentElement.classList.add("incorrect");
+  } else {
+    if (password !== repeatPassword) {
+      errors.push("Senhas não conferem");
+      password_input.parentElement.classList.add("incorrect");
+      repeat_password_input.parentElement.classList.add("incorrect");
+    }
+  }
+
+  return errors;
 }
